9ff4c681226599c310c38f19a1c22d821c721042
DeadlyAlgorythm <deadly.algorythm@gmail.com>
2022-07-18 15:50:01 +0300

    upd

--- config/api.py
+++ config/api.py
@@ -20,9 +20,6 @@ import json
 dir_path = Path.cwd()
 PATH_TO_JSON_FILE = Path(dir_path, './config/history.json')
 
-def write_history(PATH_TO_JSON_FILE, data: dict) -> None:
-    with open(PATH_TO_JSON_FILE, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
 
 def home(request):
     data = {"message": "hello from json response", "num": 12.2}
@@ -50,6 +47,10 @@ class ExchangeRate:
 
 ExchangeRates = list[ExchangeRate]
 
+def write_history(PATH_TO_JSON_FILE, data) -> None:
+    with open(PATH_TO_JSON_FILE, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
 
 class ExchangeRatesHistory:
     _history: ExchangeRates = []
@@ -59,16 +60,17 @@ class ExchangeRatesHistory:
         """We woud like to add ExchangeRates instances if it is not last duplicated"""
 
         if not cls._history:
-            cls._history.append(instance)
-            write_history(cls._history)
+            cls._history.append(instance)           
         elif cls._history[-1] != instance:
             cls._history.append(instance)
 
     @classmethod
     def as_dict(cls) -> dict:
         """Main representation interface"""
-
+        data = {"results": [asdict(er) for er in cls._history]}
+        write_history(PATH_TO_JSON_FILE, data)
         return {"results": [asdict(er) for er in cls._history]}
+    
 
 
 def btc_usd(request):
